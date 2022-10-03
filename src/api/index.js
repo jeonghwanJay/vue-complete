972b8a1e41a5c5006e9470dcf46c80969facdfb6
jeonghwanJay <nawhgnoejij@naver.com>
2022-10-03 22:31:46 +0900

    edit : index ( in api folder, in routes folder)

--- src/api/index.js
+++ src/api/index.js
@@ -16,4 +16,7 @@ function fetchJobsList() {
 function fetchUserInfo(username) {
     return axios.get(`${config.baseUrl}user/${username}.json`)
 }
-export { fetchNewsList, fetchAskList, fetchJobsList, fetchUserInfo}
+function fetchCommentId(id) {
+    return axios.get(`${config.baseUrl}item/${id}.json`)
+}
+export { fetchNewsList, fetchAskList, fetchJobsList, fetchUserInfo, fetchCommentId}
