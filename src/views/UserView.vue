c263d7b952187f7719e1b2dec6b814cbadde30af
jeonghwanJay <nawhgnoejij@naver.com>
2022-10-03 18:39:27 +0900

    edit : NewsView, UserView

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    User
+    <p>name : {{ this.$store.state.user.id}}</p>
+    <p>karma : {{ this.$store.state.user.karma}}</p>
+    <p>created : {{ this.$store.state.user.created}}</p>
   </div>
 </template>
 
 <script>
 export default {
-
+  created() {
+    const username = this.$route.params.id
+    this.$store.dispatch('FETCH_USER', username)
+    console.log(this.$store.state.user);
+  }
 }
 </script>
 
