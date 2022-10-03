a8627d7779e75b8c3e90ca6bdf372bb24aba21fd
jeonghwanJay <nawhgnoejij@naver.com>
2022-10-03 14:17:38 +0900

    add : router folder, index.js edit : main.js

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import JobsView from '../views/JobsView.vue'
+import AskView from '../views/AskView.vue'
+import NewsView from '../views/NewsView.vue'
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+    routes: [
+        {
+            path: '/jobs',
+            component: JobsView,
+        },
+                {
+            path: '/ask',
+            component: AskView,
+        },
+                        {
+            path: '/news',
+            component: NewsView,
+        }
+    ]
+})
