4cd9ae2213a4b4680765874d5d98bdcc00839d20
jeonghwanJay <nawhgnoejij@naver.com>
2022-10-03 17:22:05 +0900

    add file : mutations, actions edit : index(in store folder)

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import vuex from 'vuex'
-import {fetchAskList, fetchJobsList, fetchNewsList} from '../api/index'
+import mutations from './mutations'
+import actions from './actions'
 
 Vue.use(vuex);
 
@@ -15,32 +16,6 @@ export const store = new vuex.Store({
             return state.ask;
         }
     },
-    mutations: {
-        SET_NEWS(state, news) {
-            state.news = news;
-        },
-        SET_ASK(state, ask) {
-            state.ask = ask;
-        },
-        SET_JOBS(state, jobs) {
-            state.jobs = jobs;
-        }
-    },
-    actions: {
-        FETCH_NEWS(context) {
-            fetchNewsList()
-                .then(response => context.commit('SET_NEWS', response.data) )
-                .catch(error => console.log(error))
-        },
-        FETCH_ASK(context) {
-            fetchAskList()
-                .then(response => context.commit('SET_ASK', response.data))
-                .catch(error => console.log(error))
-        },
-        FETCH_JOBS(context) {
-            fetchJobsList()
-                .then(response => context.commit('SET_JOBS', response.data))
-                .catch(error => console.log(error))
-        },
-    },
+    mutations,
+    actions,
 })
