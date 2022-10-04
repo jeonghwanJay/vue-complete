a10a1b260521ba43e914999b99ffcf83c039c233
jeonghwanJay <nawhgnoejij@naver.com>
2022-10-04 20:50:24 +0900

    edit : AskView, JobsView ( CSS styling)

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    <p v-for="item in fetchedAsk" v-bind:key="item.data">
-      
-        <router-link v-bind:to="`item/${item.id}`">
-          {{item.title}}
-        </router-link>
-        
-      <small>{{item.time_ago}} by {{ item.user}}</small>
-    </p>
+    <ul class="news-list">
+      <li v-for="item in fetchedAsk" v-bind:key="item.data" class="post">
+        <div class="point">
+          {{item.points}}
+        </div>
+          <div>
+            <p class="news-title">
+              <router-link v-bind:to="`item/${item.id}`">
+                {{item.title}}
+              </router-link>
+            </p>
+          <small class="link-text">{{item.time_ago}} by 
+            <router-link v-bind:to="`user/${item.user}`" class="link-text">{{ item.user}}</router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -25,6 +34,30 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+  
+}
+.post {
+  display: flex;
+  list-style: none;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.point {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
 </style>
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,10 +1,29 @@
 <template>
   <div>
+    <ul class="news-list">
+      <li v-for="job in this.$store.state.jobs" v-bind:key="job.data" class="post">
+        <div class="point">
+          {{job.points || 0}}
+        </div>
+          <div>
+            <p class="news-title">
+              <a :href="job.url">{{job.title}}</a>
+            </p>
+          <small class="link-text">{{job.time_ago}} by 
+            <a :href="job.url">{{job.domain}}</a>
+          </small>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+
+  <!-- <div>
     <p v-for="job in this.$store.state.jobs" v-bind:key="job.data">
       <a v-bind:href="job.url">{{job.title}}</a>
       <small>{{job.time_ago}} {{ job.domain}}</small>
     </p>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -15,6 +34,30 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+  
+}
+.post {
+  display: flex;
+  list-style: none;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.point {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
 </style>
