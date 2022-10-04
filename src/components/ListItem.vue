7e8c2a096a96dda98021c9abb5a64ebc68673fcc
jeonghwanJay <nawhgnoejij@naver.com>
2022-10-04 21:12:54 +0900

    add file : ListItem.vue

---
+++ src/components/ListItem.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <ul class="news-list">
+      <li v-for="item in this.$store.state.news" v-bind:key="item.data" class="post">
+        <div class="point">
+          {{item.points}}
+        </div>
+          <div>
+            <p v-bind:href="item.url" class="news-title">
+               <a v-bind:href="item.url">{{item.title}}</a>
+            </p>
+          <small class="link-text">{{item.time_ago}} by 
+            <router-link v-bind:to="`user/${item.user}`" class="link-text">{{ item.user}}</router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    const name = this.$route.name;
+    if(name === 'news') {
+        this.$store.dispatch('FETCH_NEWS')
+    } else if (name == 'ask') {
+        this.$store.dispatch('FETCH_ASK')
+    } else if (name == 'jobs') {
+        this.$store.dispatch('FETCH_JOBS')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+  
+}
+.post {
+  display: flex;
+  list-style: none;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.point {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
+</style>
