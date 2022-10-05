094fb52e7896162a34e829f5c4f47f4a58f23f4f
jeonghwanJay <nawhgnoejij@naver.com>
2022-10-05 21:16:59 +0900

    edit : UserProfile, AskView

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <section>
+      <div class="user-container">
+        <div>
+          <i class="fa-solid fa-user"></i>
+        </div>
+        <div class="user-description">
+         <div>
+            {{info.id}}
+         </div>
+          <div class="time">
+            {{ info.created}}
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        info: Object,
+    }
+}
+</script>
+
+<style scoped>
+.user-container {
+  display: flex;
+  padding: 0.5rem;
+  align-items: center;
+}
+.fa-solid {
+  font-size: 2rem;
+}
+.user-description {
+  margin-left: 8px;
+}
+.time {
+  font-size: 0.8rem;
+}
+</style>
--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,22 +1,5 @@
 <template>
   <div>
-    <!-- <ul class="news-list">
-      <li v-for="item in fetchedAsk" v-bind:key="item.data" class="post">
-        <div class="point">
-          {{item.points}}
-        </div>
-          <div>
-            <p class="news-title">
-              <router-link v-bind:to="`item/${item.id}`">
-                {{item.title}}
-              </router-link>
-            </p>
-          <small class="link-text">{{item.time_ago}} by 
-            <router-link v-bind:to="`user/${item.user}`" class="link-text">{{ item.user}}</router-link>
-          </small>
-        </div>
-      </li>
-    </ul> -->
     <list-item></list-item>
   </div>
 </template>
@@ -33,36 +16,6 @@ export default {
   components: {
     ListItem,
   }
-//   created() {
-//     this.$store.dispatch('FETCH_ASK')
-// }
 }
 </script>
 
-<style scoped>
-/* .news-list {
-  margin: 0;
-  padding: 0;
-  
-}
-.post {
-  display: flex;
-  list-style: none;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-}
-.point {
-  width: 80px;
-  height: 60px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: #42b883;
-}
-.news-title {
-  margin: 0;
-}
-.link-text {
-  color: #828282;
-} */
-</style>
