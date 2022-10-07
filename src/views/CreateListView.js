bf0add28f83ba807792caba228496481e626b72c
jeonghwanJay <nawhgnoejij@naver.com>
2022-10-07 21:13:01 +0900

    add : ListView.vue, CreateListView.js

---
+++ src/views/CreateListView.js
@@ -0,0 +1,19 @@
+import ListView from './ListView.vue'
+import bus from '../utils/bus'
+export default function createListView(name) {
+    return {
+        name,
+          created() {
+            bus.$emit('start:spinner')
+            setTimeout(() => {
+            this.$store.dispatch('FETCH_LIST')
+            .then(() => bus.$emit('end:spinner'))
+            .catch((error) => console.log(error))
+            }, 3000)
+        },
+        render(createElement) {
+            return createElement(ListView)
+        }
+    }
+    
+}
