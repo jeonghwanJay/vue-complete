bf38e42d391b3bd099e330a875ae6d14b85cd4ce
jeonghwanJay <nawhgnoejij@naver.com>
2022-10-08 15:27:50 +0900

    edit : ListMixin, CreateListView

--- src/views/CreateListView.js
+++ src/views/CreateListView.js
@@ -1,18 +1,18 @@
-import ListView from './ListView.vue'
-import bus from '../utils/bus'
-export default function createListView(pageName) {
-    return {
-        pageName,
-          created() {
-                bus.$emit('start:spinner')
-            setTimeout(() => {
-                this.$store.dispatch('FETCH_LIST', this.$route.name)
-                .then(() => bus.$emit('end:spinner'))
-                .catch((error) => console.log(error))
-            }, 3000)
-        },
-        render(createElement) {
-            return createElement(ListView)
-        }
-    }
-}
+// import ListView from './ListView.vue'
+// import bus from '../utils/bus'
+// export default function createListView(pageName) {
+//     return {
+//         pageName,
+//           created() {
+//                 bus.$emit('start:spinner')
+//             setTimeout(() => {
+//                 this.$store.dispatch('FETCH_LIST', this.$route.name)
+//                 .then(() => bus.$emit('end:spinner'))
+//                 .catch((error) => console.log(error))
+//             }, 3000)
+//         },
+//         render(createElement) {
+//             return createElement(ListView)
+//         }
+//     }
+// }
