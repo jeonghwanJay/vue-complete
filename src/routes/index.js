bde8ced16bb95ca52a4fe7afde4a95a6dc655980
jeonghwanJay <nawhgnoejij@naver.com>
2022-10-08 13:56:19 +0900

    edit : index(in routes folder)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import UserView from '../views/UserView.vue'
+import NewsView from '../views/NewsView.vue'
+import JobsView from '../views/JobsView.vue'
+import AskView from '../views/AskView.vue'
 import ItemView from '../views/ItemView.vue'
-import createListView from '../views/CreateListView'
+// import createListView from '../views/CreateListView'
 
 Vue.use(VueRouter);
 
@@ -16,17 +19,20 @@ export const router = new VueRouter({
         {
             path: '/jobs',
             name: 'jobs',
-            component: createListView('JobsView')
+            // component: createListView('JobsView')
+            component: JobsView
         },
                 {
             path: '/ask',
             name: 'ask',
-            component: createListView('AskView')
+            // component: createListView('AskView')
+            component: AskView
         },
                         {
             path: '/news',
             name: 'news',
-            component: createListView('NewView')
+            // component: createListView('NewView')
+            component: NewsView
         },
         {
             path: '/user/:id',
